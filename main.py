b92bc3a690c1580b279fe8ad60212a181019ef31
Илья <isplatonov@miem.hse.ru>
2021-06-24 01:47:36 +0300

    Fractal branch

--- main.py
+++ main.py
@@ -3,23 +3,39 @@ import turtle as ttl
 ttl.color('green')
 ttl.speed(0)
 ttl.setworldcoordinates(-1, -1, 2000, 2000)
+ttl.setheading(45)
 ttl.delay(0)
 ttl.tracer(0, 0)
 
-rules = {'A': 'B-A-B',
-         'B': 'A+B+A',
-         '+': '+',
-         '-': '-'}
+positions = list()
+angles = list()
 
-draw = {'A': lambda: ttl.fd(20),
-        'B': lambda: ttl.fd(20),
-        '+': lambda: ttl.left(60),
-        '-': lambda: ttl.right(60)}
+rules = {'X': 'F-[[X]+X]+F[+FX]-X',
+         'F': 'FF',
+         '+': '+',
+         '-': '-',
+         '[': '[',
+         ']': ']'}
+
+def get_pos_n_angle():
+    positions.append(ttl.pos())
+    angles.append(ttl.heading())
+
+def set_pos_n_angle():
+    ttl.setpos(positions.pop())
+    ttl.setheading(angles.pop())
+
+draw = {'F': lambda: ttl.fd(10),
+        'X': lambda: ttl.fd(0),
+        '-': lambda: ttl.left(25),
+        '+': lambda: ttl.right(25),
+        '[': get_pos_n_angle,
+        ']': set_pos_n_angle}
 
 # Number of iterations
 n = 6
 # Input string
-s = 'A'
+s = 'X'
 
 print(set(s) and set(draw.keys()))
 if (set(s) and set(draw.keys())) != set(draw.keys()):
