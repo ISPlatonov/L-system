f013043e901789760126cf428f43a7a96ec92a3f
Илья <isplatonov@miem.hse.ru>
2021-06-30 01:45:46 +0300

    some random in angles are added

--- main.py
+++ main.py
@@ -1,42 +1,63 @@
 import turtle as ttl
+from random import randint
 
 ttl.color('green')
 ttl.speed(0)
-ttl.setworldcoordinates(0, 0, 2000, 2000)
-ttl.setheading(45)
+ttl.setworldcoordinates(-1000, 0, 2000, 2000)
+ttl.setheading(90)
 ttl.delay(0)
 ttl.tracer(0, 0)
 ttl.hideturtle()
+ttl.pensize(10)
 
 positions = list()
 angles = list()
+pensizes = list()
+lengths = list()
 
-rules = {'X': 'F-[[X]+X]+F[+FX]-X',
-         'F': 'FF',
+rules = {'X': 'F[@[-X]+X]',
+         'F': 'F',
          '+': '+',
          '-': '-',
          '[': '[',
-         ']': ']'}
+         ']': ']',
+         '@': '@'}
 
-def get_pos_n_angle():
+def get_data():
+    global l
     positions.append(ttl.pos())
     angles.append(ttl.heading())
+    pensizes.append(ttl.pensize())
+    lengths.append(l)
 
-def set_pos_n_angle():
+def set_data():
+    global l
     ttl.setpos(positions.pop())
     ttl.setheading(angles.pop())
-
-draw = {'F': lambda: ttl.fd(10),
-        'X': lambda: ttl.fd(0),
-        '-': lambda: ttl.left(25),
-        '+': lambda: ttl.right(25),
-        '[': get_pos_n_angle,
-        ']': set_pos_n_angle}
+    ttl.pensize(pensizes.pop())
+    l = lengths.pop()
 
 # Number of iterations
-n = 6
+n = 11
 # Input string
 s = 'X'
+# Lines start length
+l = 200
+
+def get_child_lines():
+    global l
+    if ttl.pensize() > 0:
+        ttl.pensize(ttl.pensize() - 1)
+    if l > 60:
+        l -= 20
+
+draw = {'F': lambda: ttl.fd(l),
+        'X': lambda: ttl.fd(l),
+        '-': lambda: ttl.left(randint(0, 45)),
+        '+': lambda: ttl.right(randint(0, 45)),
+        '[': get_data,
+        ']': set_data,
+        '@': get_child_lines}
 
 print(set(s) and set(draw.keys()))
 if (set(s) and set(draw.keys())) != set(draw.keys()):
